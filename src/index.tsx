9b09167aa6e2cd64929751f6000f0d87594107fb
Renz Jaromay <lmjaromay@gmail.com>
2022-07-14 14:19:38 +0800

    feat: implement auth0 to login

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+
+import { AppState, Auth0Provider, Auth0ProviderOptions } from "@auth0/auth0-react";
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const auth0 = {
+  domain: process.env.REACT_APP_DOMAIN || "",
+  clientId: process.env.REACT_APP_CLIENT_ID || "",
+  redirectUri: (process.env.REACT_APP_URL || "http://localhost:3000") + "/home"
+}
+
+const Auth0ProviderWithRedirectCallback = ({
+  children,
+  ...props
+}: PropsWithChildren<Auth0ProviderOptions>) => {
+  const navigate = useNavigate();
+
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate((appState && appState.returnTo) || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider onRedirectCallback={onRedirectCallback} {...props}>
+      {children}
+    </Auth0Provider>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +39,14 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Auth0ProviderWithRedirectCallback
+        domain={auth0.domain}
+        clientId={auth0.clientId}
+        redirectUri={auth0.redirectUri}
+        scope="read:users"
+      >
+        <App />
+      </Auth0ProviderWithRedirectCallback>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,18 +1,25 @@
-
 import { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
 
+
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 interface LandingPageProps {
     
 }
- 
+
+const LoginButton = () => {
+    const { loginWithRedirect } = useAuth0();
+  
+    return <button className="btn btn-primary" onClick={() => loginWithRedirect()}>Log In</button>;
+  };
+
 const LandingPage: FunctionComponent<LandingPageProps> = () => {
     return ( 
             <div className="container-sm w-75 p-4 mt-4">
                 <p>Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city</p>
-                <Link to="/home"><button className="btn btn-primary mt-2">Login</button></Link>
+                {/* <Link to="/home"><button className="btn btn-primary mt-2">Login</button></Link> */}
+                <LoginButton />
             </div>
         );
 }
